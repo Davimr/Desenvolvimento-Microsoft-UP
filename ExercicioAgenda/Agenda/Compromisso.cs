37820d6d53f6872801a353e70ae3b9c6d7ff93c0
1827524 <1827524@DASO113959>
2019-09-02 19:37:37 -0300

    commit classes

---
+++ ExercicioAgenda/Agenda/Compromisso.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda
+{
+    public class Compromisso
+    {
+        [Required]
+        [MinLength(5)]
+        [MaxLength(200)]
+        public String Titulo { get; set; }
+
+        [MaxLength(500)]
+        public String Local { get; set; }
+
+        [Required]
+        public DateTime DataInicio { get; set; }
+        public DateTime DataFim { get; set; }
+
+        [InverseProperty("Compromissos")]
+        public IList<Contato> Participantes { get; set; }
+
+
+
+    }
+}
--- ExercicioAgenda/Agenda/Contato.cs
+++ ExercicioAgenda/Agenda/Contato.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Agenda
 {
-    class Contato
+    public class Contato
     {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public String Nome { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(100)]
+        public String Sobrenome { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        [MaxLength(20)]
+        public String Telefone { get; set; }
+
+        [InverseProperty("Participantes")]
+        public IList<Compromisso> Compromissos { get; set; }
+
+
+
     }
 }
